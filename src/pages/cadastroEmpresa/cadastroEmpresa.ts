f34c736379dffe959d6f16649df3740a3a650413
Luis Antônio Neto <luisantonioneto@gmail.com>
2017-01-31 21:25:07 -0200

    Commit inicial

---
+++ src/pages/cadastroEmpresa/cadastroEmpresa.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'cadastroEmpresa.html'
+})
+export class CadastroEmpresa {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
---
+++ src/pages/page1/page1.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+import { CadastroEmpresa } from '../cadastroEmpresa/cadastroEmpresa';
+
+@Component({
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  abrirCadastroEmpresa(){
+    this.navCtrl.push(CadastroEmpresa);
+  }
+
+}
